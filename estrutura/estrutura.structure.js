e5f6aa17a9d57d969ab818b1ffcd8e72f1db85c0
Bruno Gressler da Silveira <brunogressler1@gmail.com>
2022-12-10 12:06:21 -0300

    add arquivos

---
+++ estrutura/estrutura.structure.js
@@ -0,0 +1,27 @@
+const modelStructure = {
+    id: null,
+    lua: {
+        lua: false,
+        habilitarLua: false,
+    },
+    mercurio: {
+        mercurio: false,
+        habilitarMercurio: false,
+    },
+    venus: {
+        venus: false,
+        habilitarVenus: false,
+    },
+    terra: {
+        terra: false,
+        habilitarTerra: false,
+    },
+    marte: {
+        marte: false,
+        habilitarMarte: false,
+    },
+    jupter: {
+        jupter: false,
+        habilitarJupter: false,
+    },
+}
---
+++ js/head.js
@@ -0,0 +1,12 @@
+var head = "<title>VueJS</title>\n\
+            <meta charset='UTF-8'>\n\
+            <meta name='description' content='VueJS'>\n\
+            <meta name='author' content='Bruno Gressler da Ilveira'>\n\
+            <meta name='keywords' content='JavaScript, VueJS, HTML5'>\n\
+            <meta name='copyright' content='© 2022 Bruno'>\n\
+            <meta name='robots' content='all'>\n\
+            <meta name='rating' content='general'>\n\
+            <meta http-equiv='pragma' content='no-cache'>\n\
+            <meta http-equiv='content-language' content='pt-br'>\n\
+            <meta name='revisit-after' content='7 days'>";
+document.write(head);
